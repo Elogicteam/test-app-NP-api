61e91db15c5e2e6817ed149fea3f56598abfd3e2
Merkaln-Bogdan <piccasoul19@ukr.net>
2022-09-08 13:00:34 +0300

    fixed name convention some issues

---
+++ config/webpack.base.conf.js
@@ -0,0 +1,106 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlPluginRemove = require("html-webpack-plugin-remove");
+const isProduction = process.env.NODE_ENV !== "production";
+
+const PATHS = {
+  src: path.join(__dirname, "../src"),
+  dist: path.join(__dirname, "../dist"),
+};
+
+// Base Webpack config
+module.exports = {
+  externals: {
+    paths: PATHS
+  },
+  entry: {
+    app: PATHS.src,
+  },
+  output: {
+    filename: `${PATHS.assets}js/[name].[hash].js`,
+    path: PATHS.dist
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: "vendors",
+          test: /node_modules/,
+          chunks: "all",
+          enforce: true
+        }
+      }
+    }
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: "babel-loader",
+      exclude: isProduction ? [/node_modules/, /\.smart-gread-layer$/] : /node_modules/,
+    },
+    {
+      // Fonts
+      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "file-loader",
+      options: {
+        name: "[name].[ext]"
+      }
+    },
+    {
+      test: /\.(png|jpg|gif|svg)$/,
+      loader: "file-loader",
+      options: {
+        name: "[name].[ext]"
+      }
+    }, {
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        MiniCssExtractPlugin.loader,
+        {
+          loader: "css-loader",
+          options: { sourceMap: true }
+        }, {
+          loader: "postcss-loader",
+          options: { sourceMap: true, config: { path: "./postcss.config.js"} }
+        }, {
+          loader: "sass-loader",
+          options: { sourceMap: true }
+        }
+      ]
+    }, {
+      test: /\.css$/,
+      use: [
+        "style-loader",
+        MiniCssExtractPlugin.loader,
+        {
+          loader: "css-loader",
+          options: { sourceMap: true }
+        }, {
+          loader: "postcss-loader",
+          options: { sourceMap: true, config: { path: "./postcss.config.js" } }
+        }
+      ]
+    }]
+  },
+  resolve: {
+    alias: {
+      "~": PATHS.src
+    }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `${PATHS.assets}css/[name].[hash].css`,
+    }),
+    new HtmlWebpackPlugin({
+      // hash: false,
+      inject: true,
+      template: `${PATHS.src}/index.html`,
+      filename: "./index.html"
+    }),
+    new HtmlPluginRemove(/<!--deletestart-->[\s\S]*<!--deleteend-->/gi),
+    new CopyWebpackPlugin([])
+  ],
+};
